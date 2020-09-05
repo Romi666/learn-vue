b9d72b0c87ee9f030aec1772479e3633602f98a4
Romi Abdul Yusuf <romi161199@gmail.com>
2020-09-05 08:41:31 +0700

    events

--- lesson-1/app.js
+++ lesson-1/app.js
@@ -3,11 +3,24 @@ new Vue({
     data: {
         name: "Ninja",
         website : "https://www.facebook.com/",
-        websiteTag : "<a href=https://www.facebook.com/>Web Tag</a>"
+        websiteTag : "<a href=https://www.facebook.com/>Web Tag</a>",
+        age : 25,
+        x : 0,
+        y : 0,
     },
     methods:{
         greet: function(event) {
             return 'Good ' + event + ' '+this.name
+        },
+        Add: function(inc) {
+            this.age += inc;
+        },
+        Subtract: function(dec){
+            this.age -= dec;
+        },
+        updateXY: function(event){
+            this.x = event.offsetX;
+            this.y = event.offsetY 
         }
     }
 })
